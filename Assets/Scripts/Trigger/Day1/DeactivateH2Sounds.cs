eb8bb21371831fcd59988e95d7cfdd46e81985b2
ducampcarlos <ducampcarlos@gmail.com>
2024-09-01 15:37:21 -0300

    Fix: Se saco el tag a la puerta a H2 asi no genera Excepciones.

--- Assets/Scripts/Trigger/Day1/DeactivateH2Sounds.cs
+++ Assets/Scripts/Trigger/Day1/DeactivateH2Sounds.cs
@@ -7,6 +7,7 @@ public class DeactivateH2Sounds : MonoBehaviour, IActivable
     public void Activate()
     {
         Day1Events.Instance.DeactivateH2Sound();
+        gameObject.tag = "Untagged";
         Destroy(this);
     }
 }
