18e91545a1f000861903c29523f68f8b2caabd78
Ign4R <69459950+Ign4R@users.noreply.github.com>
2024-09-12 15:41:14 -0300

    stop blow with door

--- Assets/Scripts/Trigger/Day1/DeactivateH2Sounds.cs
+++ Assets/Scripts/Trigger/Day1/DeactivateH2Sounds.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class DeactivateH2Sounds : MonoBehaviour, IActivable
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        SoundManager.main.Stop("Blows");
-    }
     public void Activate()
     {
         SoundManager.main.Play("Alarm");
+        SoundManager.main.Stop("Blows");
         Day1Events.Instance.DeactivateH2Sound();
         gameObject.tag = "Untagged";
         Destroy(this);
